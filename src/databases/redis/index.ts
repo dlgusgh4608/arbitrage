4293be7d98cc24be9e76bfd7fca23fb50149f9db
dlgusgh4608 <dlgusgh4608@gmail.com>
2024-09-24 14:07:33 +0900

    Add: Redis connect

---
+++ src/databases/redis/index.ts
@@ -0,0 +1,117 @@
+import Redis from 'ioredis';
+
+class RedisList {
+  #client: Redis
+  #key: string
+
+  constructor(key: string, client: Redis) {
+    this.#client = client
+    this.#key = key
+  }
+  
+  async get<T extends object>(startIndex: number, endIndex: number): Promise<T[]> {
+    try {
+      const response = await this.#client.lrange(this.#key, startIndex, endIndex)
+      return response.map((item) => JSON.parse(item)) as T[]
+    } catch (error) {
+      throw error
+    }
+  }
+  
+  async getAll<T extends object>(): Promise<T[]> {
+    try {
+      const response = await this.#client.lrange(this.#key, 0, -1)
+      return response.map((item) => JSON.parse(item)) as T[]
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async push<T extends object>(data: T): Promise<number> {
+    try {
+      const response = await this.#client.rpush(this.#key, JSON.stringify(data))
+      return response
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async unshift<T extends object>(data: T): Promise<number> {
+    try {
+      const response = await this.#client.lpush(this.#key, JSON.stringify(data))
+      return response
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async shift<T extends object>(): Promise<T | null> {
+    try {
+      const response = await this.#client.lpop(this.#key)
+
+      if (response === null) return null
+
+      return JSON.parse(response)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async pop<T extends object>(): Promise<T | null> {
+    try {
+      const response = await this.#client.rpop(this.#key)
+
+      if (response === null) return null
+
+      return JSON.parse(response)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async popMultiple<T extends object>(count: number): Promise<T[]> {
+    try {
+      const result: T[] = [];
+
+      for (let i = 0; i < count; i++) {
+        const response = await this.shift<T>();
+        if (response === null) break;
+
+        result.push(response);
+      }
+      return result
+    } catch (error) {
+      throw error
+    }
+  }
+  
+  async delete() {
+    try {
+      await this.#client.del(this.#key)
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+class RedisClient {
+  #client: Redis;
+
+  constructor() {
+    this.#client = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT || '6379'),
+      password: process.env.REDIS_PASSWORD,
+    });
+  }
+
+  async close(): Promise<void> {
+    await this.#client.quit();
+  }
+
+  list(key: string): RedisList {
+    return new RedisList(key, this.#client)
+  }
+}
+
+export const redisClient = new RedisClient();
