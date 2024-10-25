f4080ee1a2d8e6e8566fbf281909b5e967e6a402
dlgusgh4608 <dlgusgh4608@gmail.com>
2024-10-25 14:26:43 +0900

    FIx: aws rds connect error, psql connect is normal but npm pg module error. I do not know why

--- src/databases/pg/index.ts
+++ src/databases/pg/index.ts
@@ -1,9 +1,12 @@
 import { Pool } from 'pg'
 
+const ssl = process.env.NODE_ENV === 'production' ? { ssl: { rejectUnauthorized: false } } : {}
+
 export const pool = new Pool({
   host: process.env.PG_HOST,
   port: parseInt(process.env.PG_PORT || '5432'),
   user: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DB_NAME,
+  ...ssl
 })
